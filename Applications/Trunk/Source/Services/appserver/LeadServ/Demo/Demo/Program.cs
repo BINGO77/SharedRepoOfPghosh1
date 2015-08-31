5e9ace6575afe98dbf6e46f0d886f0403f9e3516
unknown <360850@PC257662.cts.com>
2015-08-31 19:03:37 +0530

    Comit - changes in Program.cs in Branch

--- Applications/Trunk/Source/Services/appserver/LeadServ/Demo/Demo/Program.cs
+++ Applications/Trunk/Source/Services/appserver/LeadServ/Demo/Demo/Program.cs
@@ -17,6 +17,7 @@ namespace Demo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Ewn(abc);
         }
     }
 }
